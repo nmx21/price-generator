c5cf080b07ee47c494c34a1414c13956cc88c1ac
Maks <maksim.stepanok@gmail.com>
2023-03-20 12:25:08 +0200

    - Optimization and refactoring

--- src/main/java/com/price/price/generator/service/XLSXFileParcer.java
+++ src/main/java/com/price/price/generator/service/XLSXFileParcer.java
@@ -4,6 +4,7 @@ import com.price.price.generator.model.Device;
 import com.price.price.generator.model.Goods;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -25,39 +26,17 @@ public class XLSXFileParcer {
             if (row != null) {
                 int lastCellNum = row.getLastCellNum();
                 if (lastCellNum > 0) {
-                    String categoryProduct;
-                    String nameProduct;
-                    String code1Product;
-                    String code2Product;
 
-                    if (row.getCell(0) == null) {
-                        categoryProduct = "";
-                    } else {
-                        categoryProduct = row.getCell(0).toString();
-                    }
-
-                    if (row.getCell(1) == null) {
-                        nameProduct = "";
-                    } else {
-                        nameProduct = row.getCell(1).toString();
-                    }
-                    if (row.getCell(2) == null) {
-                        code1Product = "";
-                    } else {
-                        code1Product = row.getCell(2).toString();
-                    }
-                    if (row.getCell(3) == null) {
-                        code2Product = "";
-                    } else {
-                        code2Product = row.getCell(3).toString();
-                    }
-
+                    String categoryProduct = getValueCell(row.getCell(0));
+                    String nameProduct = getValueCell(row.getCell(1));
+                    String code1Product = getValueCell(row.getCell(2));
+                    String code2Product = getValueCell(row.getCell(3));
 
                     List<String> codeList = new ArrayList<>();
-                    if (code1Product != null && !code1Product.trim().isEmpty()) {
+                    if ( !code1Product.isEmpty()) {
                         codeList.add(code1Product);
                     }
-                    if (code2Product != null && !code2Product.trim().isEmpty()) {
+                    if ( !code2Product.isEmpty()) {
                         codeList.add(code2Product);
                     }
                     if (codeList.isEmpty()) {
@@ -65,13 +44,13 @@ public class XLSXFileParcer {
                     }
 
                     Device device = new Device();
-                    if (nameProduct != null && !nameProduct.trim().isEmpty()) {
+                    if ( !nameProduct.isEmpty()) {
                         device.setDescription(nameProduct);
                         device.setCode(codeList);
                     }
 
                     Goods goods = new Goods();
-                    if (categoryProduct != null && !categoryProduct.trim().isEmpty()) {
+                    if ( !categoryProduct.isEmpty()) {
                         goods.setCategory(categoryProduct);
                         goods.setDevice(device);
                         goodsList.add(goods);
@@ -80,4 +59,13 @@ public class XLSXFileParcer {
             }
         }
     }
+
+    private static String getValueCell(XSSFCell cell) {
+        if (cell == null) {
+            return  "";
+        } else {
+            return  cell.toString().trim();
+        }
+    }
+
 }
